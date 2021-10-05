8eaeafc8cc2e9e031875ced91aa76b158e546a76
Hjenry <evelyst21@gmail.com>
2021-10-05 18:03:58 +0800

    edited proxy

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -9,7 +9,7 @@ import {
   import { setContext } from "@apollo/client/link/context";
 
 const httpLink = createHttpLink({
-    uri: 'http://localhost:5000'
+    uri: 'https://infinite-coast-89533.herokuapp.com/'
 });
 
 const authLink = setContext(() => {
